1b8bd45ef6db067cb2c75c118f13515da66e1ba1
aadil124 <ansariaadil124@gmail.com>
2023-03-30 16:06:14 +0530

    styles added

--- memoryGame/memoryGame.js
+++ memoryGame/memoryGame.js
@@ -50,17 +50,48 @@ const cardArray = [
 ];
 
 // console.log(cardArray);
-
+cardArray.sort(() => 0.5 - Math.random());
 const grid = document.querySelector("#grid");
 const results = document.querySelector("#results");
 // console.log(grid);
+
+let cardsChosen = [];
+let cardsChosenId = [];
 
 function createBoard() {
   for (let counter = 0; counter < cardArray.length; counter++) {
     const card = document.createElement("img");
     card.setAttribute("src", "images/blank.png");
     card.setAttribute("data-id", counter);
-    console.log(card);
+    // console.log(card);
+    card.addEventListener("click", flipCard);
+    grid.appendChild(card);
+  }
+}
+
+function flipCard() {
+  // console.log(cardArray)
+  let cardId = this.getAttribute("data-id");
+  //   console.log("clikded", cardId);
+  cardsChosen.push(cardArray[cardId].name);
+  // console.log("cardsChosen", cardsChosen)
+  cardsChosenId.push(cardId);
+  //   console.log("cardsChosenId", cardsChosenId)
+  // console.log(this)
+  this.setAttribute("src", cardArray[cardId].img);
+}
+
+function checkTheMatch() {
+  const cards = document.querySelector("img");
+  const optionOneId = cardsChosenId[0];
+  const optionTwoId = cardsChosenId[1];
+
+  if (optionOneId == optionTwoId) {
+    alert("You have found the match");
+    cards[optionOneId].setAttribute("src", "images/white.png");
+    cards[optionTwoId].setAttribute("src", "images/white.png");
+     cards[optionOneId].removeEventListener("click", flipCard);
+     cards[optionTwoId].removeEventListener("click", flipCard);
   }
 }
 createBoard();
