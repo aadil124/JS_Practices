bedd05cba1fde43e29c015d8997f7c1760ff550e
aadil124 <ansariaadil124@gmail.com>
2023-04-01 18:34:06 +0530

    added query selector

--- dom/dom.js
+++ dom/dom.js
@@ -1,4 +1,8 @@
 let myDiv = document.getElementById("myDiv");
 let myText = myDiv.textContent;
 
-console.log(myText);
+// console.log(myText);
+
+const textContents = document.querySelector("p");
+const innerText = textContents.innerText;
+console.log(innerText);
