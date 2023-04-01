4079effe981190bef5fd7543693844f3a30a05e7
aadil124 <ansariaadil124@gmail.com>
2023-04-01 18:36:53 +0530

    updated dom

--- dom/dom.js
+++ dom/dom.js
@@ -4,5 +4,7 @@ let myText = myDiv.textContent;
 // console.log(myText);
 
 const textContents = document.querySelector("p");
-const innerText = textContents.innerText;
-console.log(innerText);
+const innerTexts = textContents.innerText;
+// console.log(innerTexts);
+
+console.log(innerTexts);
