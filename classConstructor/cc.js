0d88da5202edfa2156644c7a796d09e464592b96
aadil124 <ansariaadil124@gmail.com>
2023-04-03 12:50:24 +0530

    added food name

--- classConstructor/cc.js
+++ classConstructor/cc.js
@@ -1,4 +1,15 @@
-class foodItems {
- constructor(foodItems) {}
- 
+class FoodItems {
+  constructor(foodname, foodcolor) {
+    this.foodName = foodname;
+    this.foodColor = foodcolor;
+  }
 }
+
+var food1 = new FoodItems("Banana", "yellow");
+console.log(food1);
+var food2 = new FoodItems("Orange", "orange");
+console.log(food2);
+var food3 = new FoodItems("Apple", "red");
+console.log(food3);
+var food4 = new FoodItems("Chiku", "Brown");
+console.log(food4);
