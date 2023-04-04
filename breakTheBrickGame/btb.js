36667e6cfdc62fc8fbebf8a2041c778b3f8e6a68
aadil124 <ansariaadil124@gmail.com>
2023-04-04 16:48:46 +0530

    added move ball and blocks

--- breakTheBrickGame/btb.js
+++ breakTheBrickGame/btb.js
@@ -1,9 +1,13 @@
 const grid = document.querySelector(".grid");
 const blockWidth = 100;
 const blockHeight = 20;
+const gridWidth = 560;
 
-const startUser = [200, 10];
+const startUser = [230, 10];
 let currentPosition = startUser;
+
+const startBall = [270, 40];
+let ballCurrentPosition = startBall;
 
 //create the blocks
 
@@ -60,6 +64,56 @@ addBlocks();
 //add user
 const user = document.createElement("div");
 user.classList.add("user");
-user.style.left = currentPosition[0] + "px";
-user.style.left = currentPosition[1] + "px";
-user.appendChild(user);
+drawUser();
+grid.appendChild(user);
+
+function drawUser() {
+  user.style.left = currentPosition[0] + "px";
+  user.style.bottom = currentPosition[1] + "px";
+}
+
+//draw ball
+function drawBall() {
+  ball.style.left = ballCurrentPosition[0] + "px";
+  ball.style.bottom = ballCurrentPosition[1] + "px";
+}
+
+//mover
+
+function moveUser(u) {
+  switch (u.key) {
+    case "ArrowLeft":
+      if (currentPosition[0] > 0) {
+        currentPosition[0] -= 10;
+        //cp =cp -10 = 230-10 = 220
+        drawUser();
+      }
+      break;
+    case "ArrowRight":
+      if (currentPosition[0] < gridWidth - blockWidth) {
+        currentPosition[0] += 10;
+        drawUser();
+      }
+      break;
+
+    default:
+      break;
+  }
+}
+
+// move ball
+
+function moveBall() {
+  ballCurrentPosition[0] += 2;
+  ballCurrentPosition[1] += 2;
+  drawBall();
+}
+setInterval(moveBall,30);
+
+//draw ball
+const ball = document.createElement("div");
+ball.classList.add("ball");
+drawBall();
+grid.appendChild(ball);
+
+// document.addEventListener("keydown", moveUser);
