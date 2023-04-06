43dea457a1d9f71d53a2d0b31e5643589b391165
aadil124 <ansariaadil124@gmail.com>
2023-04-07 00:57:27 +0530

    fibonnacci series using all loop

--- FibonacciSeries/fibonacciSeries.js
+++ FibonacciSeries/fibonacciSeries.js
@@ -1,33 +1,56 @@
-// console.log("helooo111111");
+/************************************Using For Loop Start*********************************/
 
-var a = 0;
-var b = 1;
-var c;
-
-const num = prompt("Please enter a limit for series");
-//  document.getElementById("fbseries").innerHTML = a;
-console.log("series: ");
-
-for (let i = 1; i <= num; i++) {
-  console.log(a);
-  document.getElementById("fbseries").innerHTML = a;
-  c = a + b;
-  a = b;
-  b = c;
-}
-
-
-// const number = parseInt(prompt("Enter the number of terms: "));
-// let n1 = 0,
+// declaration of the variables
+// var n1 = 0,
 //   n2 = 1,
-//   nextTerm;
+//   next_num,
+//   i;
+// var num = parseInt(prompt(" Enter the limit for Fibonacci Series "));
 
-// console.log("Fibonacci Series:");
-
-// for (let i = 1; i <= number; i++) {
-//   console.log(n1);
-//   nextTerm = n1 + n2;
-//   n1 = n2;
-//   n2 = nextTerm;
+// for (i = 1; i <= num; i++) {
+//   // document.getElementById("fb").innerHTML = " <br> " + n1;
+//   document.write(" <br> " + n1); // print the n1
+//   next_num = n1 + n2; // sum of n1 and n2 into the next_num
+//   n1 = n2; // assign the n2 value into n2
+//   n2 = next_num; // assign the next_num into n2
 // }
 
+/************************************Using For Loop End*********************************/
+
+/************************************Using while loop Start*********************************/
+
+// declaration of the variables
+// var n1 = 0,
+//   n2 = 1,
+//   next_num,
+//   i = 1;
+// var num = parseInt(prompt(" Enter the limit for Fibonacci Series "));
+
+// while (i <= num) {
+//   document.write(" <br> " + n1); // print the n1
+//   next_num = n1 + n2; // sum of n1 and n2 into the next_num
+//   n1 = n2; // assign the n2 value into n2
+//   n2 = next_num; // assign the next_num into n2
+//   i++;
+// }
+
+/************************************Using while loop End*********************************/
+
+/************************************Using do while Loop Start*********************************/
+
+// declaration of the variables
+var n1 = 0,
+  n2 = 1,
+  next_num,
+  i = 1;
+var num = parseInt(prompt(" Enter the limit for Fibonacci Series "));
+
+do {
+  document.write(" <br> " + n1); // print the n1
+  next_num = n1 + n2; // sum of n1 and n2 into the next_num
+  n1 = n2; // assign the n2 value into n2
+  n2 = next_num; // assign the next_num into n2
+  i++;
+} while (i <= num);
+
+/************************************Using do while Loop End*********************************/
