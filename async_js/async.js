68d48c6d74986d823d5b0fbc9b17745b846e81a9
aadil124 <ansariaadil124@gmail.com>
2023-04-08 11:50:19 +0530

    updated call back

--- async_js/async.js
+++ async_js/async.js
@@ -8,28 +8,33 @@
 // console.log("Statement 4");
 
 //creation of request object
-const request = new XMLHttpRequest();
-// console.log(request);
 
-//setting up request
-// console.log("Setting up request", request.readyState);
+const getTodos = () => {
+  const request = new XMLHttpRequest();
+  // console.log(request);
 
-// attached to event listener to object
+  //setting up request
+  // console.log("Setting up request", request.readyState);
 
-// readystatechange is a event which fires every time a state is changed (there are five states like 0,1,2,3,4)
+  // attached to event listener to object
 
-request.addEventListener("readystatechange", () => {
-  //   console.log("Setting up request", request.readyState);
+  // readystatechange is a event which fires every time a state is changed (there are five states like 0,1,2,3,4)
 
-  if (request.readyState === 4 && request.status === 200) {
-    console.log("Setting up request", request.response);
-  }
-});
+  request.addEventListener("readystatechange", () => {
+    //   console.log("Setting up request", request.readyState);
 
-//initailization of request
-request.open("GET", "https://jsonplaceholder.typicode.com/todos/1");
-// console.log("Setting up request", request.readyState);
+    if (request.readyState === 4 && request.status === 200) {
+      console.log("Setting up request", request.response);
+    }
+  });
 
-//send request to server
-request.send();
-// console.log("Setting up request", request.readyState);
+  //initailization of request
+  request.open("GET", "https://jsonplaceholder.typicode.com/todos/1");
+  // console.log("Setting up request", request.readyState);
+
+  //send request to server
+  request.send();
+  // console.log("Setting up request", request.readyState);
+};
+
+getTodos();
