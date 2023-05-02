e798cf4a18551612df438a570ea7b43092cfe178
aadil124 <ansariaadil124@gmail.com>
2023-05-03 00:21:17 +0530

    prime number

--- PracticeQuestions/primeNumber/primeNumber.js
+++ PracticeQuestions/primeNumber/primeNumber.js
@@ -1,11 +1,7 @@
-
-
 // 2.Write a logic to print all prime number from 2 to 50
 
-// for(let i = 2; i<=50; i++){
-//     if(){
-
-//     }
-// }
-
-
+for (let i = 2; i <= 50; i++) {
+  if (50 % i == 0) {
+    console.log(i);
+  }
+}
