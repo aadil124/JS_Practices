e862e5a3fa1ad8fd8ae0e7fb4c33c7e0796e5adc
aadil124 <ansariaadil124@gmail.com>
2023-05-02 12:28:48 +0530

    es6 all concepts

---
+++ ES6/es6.js
@@ -0,0 +1,64 @@
+// Ecmascript-> React
+// introduction to ES6 - Ecmascript 2015
+
+// javascript -> brandon eich -> netscape-> mozilla browser->
+// LiveScript -> 1995
+
+// microsoft -> Jscript
+
+// ECMA - European Computer Manufacturer Association ->
+
+// ecmascript1-> 1997
+// ecmascript5-> for each, array methods , map , filter, reduce
+
+// ecmascript 6 -> Es6 or Ecmascript 2015
+// new variable let const
+// var vs let vs const
+// array and object destructuring
+// spread operator
+// rest parameter
+// loops-> for in vs for off
+// Template literal
+// Arrow function
+// symbols
+// classes
+// Closures
+
+//Babel is a JavaScript Compiler ->
+// typescript
+// coffescript
+
+//converts your javascript code your of version into plane js code of older version
+
+// console.log(num);  due to hoisting
+// var num;
+
+// Hoisting :->
+// var -> it is traditional keyword in js which has scope of functional or global scope
+// u can access the data throughout the function body
+
+// Es 6
+// let
+// block Scope : let wont allow to access the variables before declaration.
+
+// console.log(num);  // 6.js:44 Uncaught ReferenceError: Cannot access 'num' before initialization
+// let num;
+
+// examples
+// 1)
+// var num = 10;
+// {
+//   var num = 20;
+//   console.log(num); // 20
+// }
+// console.log(num); //20
+
+// 2)
+// let num = 10;
+// {
+//   let num = 20;
+//   console.log(num); // 20
+// }
+// console.log(num); //10
+
+// 3)
