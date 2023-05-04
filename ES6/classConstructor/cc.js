f9d6856c2db4e9823db1bc349d2e24eebbc72033
aadil124 <ansariaadil124@gmail.com>
2023-05-04 14:30:27 +0530

    area of rectangle using oops concept

--- ES6/classConstructor/cc.js
+++ ES6/classConstructor/cc.js
@@ -100,82 +100,109 @@
 
 // examples
 
-// 1)
-class Person {
-  static companyName = "Amazon"; // static member
-  constructor(name, age, designation, salary) {
-    // set the value
-    this.name = name;
-    this.age = age;
-    this.designation = designation;
-    this.salary = salary;
-  }
+//************************************ Start example 1 **************************************/
+// class Person {
+//   static companyName = "Amazon"; // static member
+//   constructor(name, age, designation, salary) {
+//     // set the value
+//     this.name = name;
+//     this.age = age;
+//     this.designation = designation;
+//     this.salary = salary;
+//   }
 
-  // instance method
-  getData() {
-    let personData = `
-  Name: ${this.name}
-  Age: ${this.age}
-  Designation: ${this.designation}
-  Salary: ${this.salary}
-  `;
+//   // instance method
+//   getData() {
+//     let personData = `
+//   Name: ${this.name}
+//   Age: ${this.age}
+//   Designation: ${this.designation}
+//   Salary: ${this.salary}
+//   `;
 
-    console.log(personData);
-    // return data;
-  }
+//     console.log(personData);
+//     // return data;
+//   }
 
-  // static methods -> Class Name
-  static getCompanyName() {
-    console.log(Person.companyName);
-    // return Person.companyName;
-  }
-}
+//   // static methods -> Class Name se call hoga
+//   static getCompanyName() {
+//     console.log(Person.companyName);
+//     // return Person.companyName;
+//   }
+// }
 
-let person1 = new Person("Aadil", 28, "Web Developer", 25000);
+// let person1 = new Person("Aadil", 28, "Web Developer", 25000);
 
-person1.getData();
+// person1.getData();
 // console.log(person1.getData());
 
 // person1.getCompanyName();
 // console.log(person1.getCompanyName()); //caught TypeError: person1.getCompanyName is not a function
 // static funtion ko direct call nahi ker sakte hai.....us ko call kerne ke liye class ka use kerna hoga...
-Person.getCompanyName();
+// Person.getCompanyName();
 // console.log(Person.getCompanyName()); // aur agar return kare ge to yaha print hoga
 
-class employee extends Person {
-  static companyName = "Google";
-  constructor(name, age, designation, salary, location, country) {
-    super(name, age, designation, salary);
-    this.location = location;
-    this.country = country;
-  }
-  // instance methods
-  getData() {
-    let employeeData = ` Name: ${this.name}
-      Age: ${this.age}
-      Designation: ${this.designation}
-      Salary: ${this.salary}
-      Location: ${this.location}
-      Country: ${this.country}
-    `;
-    console.log(employeeData);
+// class employee extends Person {
+//   static companyName = "Google";
+//   constructor(name, age, designation, salary, location, country) {
+//     super(name, age, designation, salary);
+//     this.location = location;
+//     this.country = country;
+//   }
+//   // instance methods
+//   getData() {
+//     let employeeData = ` Name: ${this.name}
+//       Age: ${this.age}
+//       Designation: ${this.designation}
+//       Salary: ${this.salary}
+//       Location: ${this.location}
+//       Country: ${this.country}
+//     `;
+//     console.log(employeeData);
+//   }
+
+//   // static method
+//   static getCompanyName() {
+//     console.log(employee.companyName);
+//   }
+// }
+
+// let employee1 = new employee(
+//   "sameer",
+//   25,
+//   "Data Analyst",
+//   50000,
+//   "Mumbai",
+//   "India"
+// );
+// employee1.getData();
+// // employee1.getCompanyName(); //caught TypeError: employee1.getCompanyName is not a function
+
+// employee.getCompanyName();
+//************************************ End example 1 **************************************/
+
+//************************************ Start example 2 **************************************/
+// calculate area , perimeters of rectangle using oops concept
+
+class Rectangle {
+  constructor(length, breadth) {
+    this.length = length;
+    this.breadth = breadth;
   }
 
-  // static method
-  static getCompanyName() {
-    console.log(employee.companyName);
+  //logic to find area
+  getArea() {
+    return this.breadth * this.length;
   }
 }
 
-let employee1 = new employee(
-  "sameer",
-  25,
-  "Data Analyst",
-  50000,
-  "Mumbai",
-  "India"
-);
-employee1.getData();
-// employee1.getCompanyName(); //caught TypeError: employee1.getCompanyName is not a function
+let rectArea = new Rectangle(10, 20);
+console.log(` Area of Rectangle is ${rectArea.getArea()}`);
 
-employee.getCompanyName();
+class Perimeters extends Rectangle {
+
+}
+
+let 
+
+//************************************ End example 2 **************************************/
