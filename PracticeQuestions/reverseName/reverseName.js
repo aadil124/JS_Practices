12f6b8b896283fc22e04624c03904ddbbbd7682f
aadil124 <ansariaadil124@gmail.com>
2023-05-05 01:19:02 +0530

    reverse string update

--- PracticeQuestions/reverseName/reverseName.js
+++ PracticeQuestions/reverseName/reverseName.js
@@ -20,7 +20,7 @@
 
 function reverseString(str) {
   let reverseStr = "";
-  for (let i = str.length - 1; i >= 0; i--) {
+  for (let i = str.length-1; i >= 0; i--) {
     reverseStr += str[i];
   }
   return reverseStr;
