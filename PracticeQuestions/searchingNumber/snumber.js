73afd2723699a67dbef1317470be102d5b696578
aadil124 <ansariaadil124@gmail.com>
2023-05-06 11:22:32 +0530

    added 2 eg on number count

--- PracticeQuestions/searchingNumber/snumber.js
+++ PracticeQuestions/searchingNumber/snumber.js
@@ -1,4 +1,5 @@
-// list of elements given
+/*********************************** start example 1  *****************************************/
+// 1) list of elements given
 // find given number is exits or not
 
 // let array1 = [5, 4, 6, 9, 8];
@@ -13,22 +14,77 @@
 //   console.log(`${num} is not present in Array`);
 // }
 // SearchNumber(array1, 7);
+/*********************************** End example 1  *****************************************/
 
+/*********************************** start example 2  *****************************************/
 //2 )
-// 7,8,3,4,8
+//[8, 7, 6, 8, 3, 4, 8, 8, 8, 6, 9, 8, 2]
 // count of number 8
 
-function countOfNumber(arr, numToSearch) {
-  var count = 0;
+// function countOfNumber(arr, numToSearch) {
+//   let count = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] === numToSearch) {
+//       count++;
+//     }
+//   }
+//   return count;
+// }
+
+// let arr = [8, 7, 6, 8, 3, 4, 8, 8, 8, 6, 9, 8, 2];
+// let numToSearch = 8;
+// let result = countOfNumber(arr, numToSearch);
+// console.log(`count of ${numToSearch} is ${result} times`);
+/*********************************** End example 2  *****************************************/
+
+/*********************************** start example 3  *****************************************/
+//3)
+// [8, 7, 6, 8, 3, 4, 8, 8, 8, 6, 9, 8, 2]
+// print  index number of last 8
+
+// function indexNumber(arr, numToSearch) {
+//   let count = 0;
+//   let index = -1;
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] === numToSearch) {
+//       count++;
+//       if (count === 6) {
+//         index = i;
+//         break;
+//       }
+//     }
+//   }
+//   return index;
+// }
+
+// let arr = [8, 7, 6, 8, 3, 4, 8, 8, 8, 6, 9, 8, 2];
+// let numToSearch = 8;
+// let resultIndex = indexNumber(arr, numToSearch);
+// console.log(`last index value of ${numToSearch} is ${resultIndex}`);
+/*********************************** End example 3  *****************************************/
+
+/*********************************** start example 4  *****************************************/
+//3)
+// 7,8,3,4,8
+// print  index number of last 8
+
+function indexNumber(arr, numToSearch) {
+  let count = 0;
+  let index = -1;
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] === numToSearch) {
       count++;
+      if (count === 6) {
+        index = i;
+        break;
+      }
     }
   }
-  return count;
+  return arr[index];
 }
 
 let arr = [8, 7, 6, 8, 3, 4, 8, 8, 8, 6, 9, 8, 2];
 let numToSearch = 8;
-let result = countOfNumber(arr, numToSearch);
-console.log(`count of ${numToSearch} is ${result} times`);
+let resultIndex = indexNumber(arr, numToSearch);
+console.log(resultIndex);
+/*********************************** End example 4  *****************************************/
