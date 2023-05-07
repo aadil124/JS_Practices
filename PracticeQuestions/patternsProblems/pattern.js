7204dccea892581f9f2c4861f21553d7e82ccc12
aadil124 <ansariaadil124@gmail.com>
2023-05-07 11:20:14 +0530

    patterns in JS

---
+++ PracticeQuestions/patternsProblems/pattern.js
@@ -0,0 +1,12 @@
+// print X pattern
+// print hollow rectangle
+// print 1234 triangles
+
+/************************************** Start Example 1 ***************************************/
+function xPattern(n) {
+  for (let i = 1; i <= n; i++) {
+    console.log("*");
+  }
+}
+xPattern(5);
+/************************************** End Example 1   ***************************************/
