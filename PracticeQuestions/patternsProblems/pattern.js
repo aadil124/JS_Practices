1a2bfa7f08f86661f7756cc553e05002c297be29
aadil124 <ansariaadil124@gmail.com>
2023-05-08 20:09:57 +0530

    hollow rectangle

--- PracticeQuestions/patternsProblems/pattern.js
+++ PracticeQuestions/patternsProblems/pattern.js
@@ -3,10 +3,51 @@
 // print 1234 triangles
 
 /************************************** Start Example 1 ***************************************/
-function xPattern(n) {
-  for (let i = 1; i <= n; i++) {
-    console.log("*");
+
+// function xPattern(n, string) {
+//   for (let i = 1; i <= n; i++) {
+//     for (let j = 1; j <= n; j++) {
+//       if (i == j || i + j === n + 1) {
+//         string += "*";
+//       } else {
+//         string += " ";
+//       }
+//     }
+//     string += "\n";
+//   }
+// }
+let n = 5;
+for (let i = 1; i <= n; i++) {
+  let row = "";
+  for (let j = 1; j <= n; j++) {
+    if (i == j || i + j === n + 1) {
+      row += "X";
+    } else {
+      row += " ";
+    }
   }
+  // string += "\n";
+  console.log(row);
 }
-xPattern(5);
+
+// let res = xPattern(5, string);
+// console.log(string);
 /************************************** End Example 1   ***************************************/
+
+/************************************** Start Example 2 ***************************************/
+let row = "";
+let N = 5;
+for (let i = 1; i <= N; i++) {
+  for (let j = 1; j <= N; j++) {
+    if (i === 1 || j === 1 || i === N || j === N) {
+      row += "X";
+    } else {
+      row += " ";
+    }
+  }
+  row += "\n";
+  // console.log(row);
+}
+console.log(row);
+
+/************************************** End Example 2   ***************************************/
