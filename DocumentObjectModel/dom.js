09ffe21b9ac288b6999bf9f0769edb7892e9e463
aadil124 <ansariaadil124@gmail.com>
2023-05-16 00:22:38 +0530

    dom

--- DocumentObjectModel/dom.js
+++ DocumentObjectModel/dom.js
@@ -1,2 +1,20 @@
-// DOM : Document Object Model 
+// DOM : Document Object Model
 // Dom is powerful concept for creating DOM dynamic web applications where user can perform interaction.
+
+// Selectors : by ID , ClassName, TagName, querySelector , querySelectorAll
+// document.getElementById()
+// document.getElementsByClassName()
+// document.getElementsByTagName()
+// querySelector()
+// querySelectorAll()
+
+// document.createElement(p)-> create new element with given tag name
+// element.appendChild(newElement)-> add new child element to existing element.
+
+// element.addEventListener(eventType , function(){})->
+
+// dom properties
+
+// document.title -> to get title of current document
+// element.innerHTML ->set or get the content of an html element
+// element.innerText -> text like input
