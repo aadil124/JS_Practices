16b617e88071957bd0aa93772bcaa90e491d38c5
aadil124 <ansariaadil124@gmail.com>
2023-05-18 14:49:27 +0530

    updated todo list

--- PracticeQuestions/todo_app/todo.js
+++ PracticeQuestions/todo_app/todo.js
@@ -1,8 +1,18 @@
-let addTodo = document.getElementsByClassName("button")[0];
+// let addTodo = document.getElementsByClassName("button")[0];
+let addTodo = document.querySelector(".button");
 addTodo.addEventListener("click", function () {
-  let inputValue = document.getElementsByClassName("item")[0].value;
+  //   let inputValue = document.getElementsByClassName("item")[0].value;
+  let inputValue = document.querySelector(".item").value;
   //   console.log(inputValue);
-  let showResult = document.getElementsByClassName("list")[0];
+  //   let showResult = document.getElementsByClassName("list")[0];
+  let showResult = document.querySelector(".list");
   //   console.log(showResult);
-  showResult.innerHTML = `<li>${inputValue}</li>`;
+
+  //create element
+  let li = document.createElement("li");
+  li.innerText = inputValue;
+  //   showResult.appendChild(li);
+  document.querySelector(".list").appendChild(li);
+  //   document.getElementsByClassName("item")[0].value = "";
+  document.querySelector(".item").value = "";
 });
