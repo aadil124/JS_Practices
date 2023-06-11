2f7a91d3599c7c019939a19b9694f0edcffaeba9
aadil124 <ansariaadil124@gmail.com>
2023-06-11 12:21:48 +0530

    intro sync and async JS

---
+++ synchronous&AsynJS/intro/index.js
@@ -0,0 +1,118 @@
+// Synchronous and Asynchronous Programming:
+
+// Synchronous->The program execution happens line by line,where each line
+// of code waits for previous line to finish executing their code before 
+// moving on to next.
+
+// delay might happens.
+
+// Synchronous=>single lane road.
+
+// Asynchronous->It allows multiple task to be executed at the same.
+// No task is dependent on other task.
+
+// if any task is taking a longer duration->queue->waiting area.
+// it is multi lane road-
+
+// javascript has a ways to achieve asynchronous programming:
+// 1.Callback function
+// 2.Promises
+// 3.Async-await function.
+
+
+
+
+// Synchronous and Asynchronous Programming:
+
+// Synchronous->The program execution happens line by line,where each line
+// of code waits for previous line to finish executing their code before 
+// moving on to next.
+
+// delay might happens.
+
+// Synchronous=>single lane road.
+
+// Asynchronous->It allows multiple task to be executed at the same.
+// No task is dependent on other task.
+
+// if any task is taking a longer duration->queue->waiting area.
+// it is multi lane road-
+
+// javascript has a ways to achieve asynchronous programming:
+// 1.Callback function->
+// What is callback function?
+// Creation of callback function with Web apis function->
+// setTimeout->
+// setInterval->
+// Multiple callback function with dependent event.
+// What happens if we nest -->function->function-function?
+// Callback Hell->Problem->
+// Lifecycle of callstack->Message queue->Event loop->
+// Callback function execution.
+// multiple function.
+// Limitation of callback
+// Resolution to callback->Promises
+
+
+// 1.5 hr
+// 2.Promises->
+// What is promises?
+// Promise constructor and object creation.
+// Promise chaining
+// Promise Method->
+// Promise.All
+// promise.AllSettled
+// promise.Race
+
+// Promise chaining with dependent event->
+// Problems with promise concepts.
+
+
+// 8 yrs->Advance concept 
+// What are different to achieve async programming in js?-3ways
+// 3.Async-await function->
+// async 
+// await
+// fetch ,try,catch ,then,finally
+// working with API's data +
+// DOM ->
+
+
+
+
+
+
+
+
+
+
+
+
+// API->Application programming->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// API->Application programming->
+
+
+
+
+
+
+
+
